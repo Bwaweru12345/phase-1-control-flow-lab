50b8164f446268e8c99732a88bd444547c7185f4
Bwaweru12345 <bryanwaweru1076@example.com>
2023-03-20 15:49:25 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-function scuberGreetingForFeet(value){
-  // Write your code here!
-  if(value <= 400){
-      return 'This one is on me!';
-  }else if(value > 2000 && value < 2500){
-    return 'I will gladly take your thirty bucks.';
-  }else if(value > 2500){
-    return 'No can do.'
-
-  }
+function scuberGreetingForFeet(feet){
+  if (feet<=400) {return 'This one is on me!'}
+  else if (feet > 2000 && feet < 2500 ) {return 'I will gladly take your thirty bucks.'}
+  else if (feet > 2500) {return 'No can do.'}
 }
 
+let city = "NYC" 
 function ternaryCheckCity(city){
-  // Write your code here!
-  return city === 'NYC'?'Ok, sounds good.': 'No go.'
-}
-
+  if (city=== "NYC"){return "Ok, sounds good."}
+else return "No go."}
 
 function switchOnCharmFromTip(tip){
-  // Write your code here!
- if(tip === 'generous' ){
-  return 'Thank you so much.'
- }else if(tip === 'not as generous'){
-  return 'Thank you.'
- }else {
-  return 'Bye.'
- }
+  if (tip="generous"){return "Thank you so much."}
+  else if (tip = "not as generous"){return "Thank you."}
+  else  return "Bye."
 }
